2ecf634fdf4735135b2c1655c15664533c5a98ea
h2tomohiro <h2.adachi.tomohiro@gmail.com>
2021-05-03 23:37:09 -0700

    create header component file

---
+++ components/lp/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-yellow-500 h-16 flex items-center">
+      <div className="container">
+        <h1 className="text-white font-bold text-2xl">event</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
+import Header from "../components/lp/Header";
+
 const Home = () => {
-  return (
-    <header className="bg-yellow-500 h-16 flex items-center">
-      <div className="container">
-        <h1 className="text-white font-bold text-2xl">event</h1>
-      </div>
-    </header>
-  );
+  return <Header />;
 };
 
 export default Home;
