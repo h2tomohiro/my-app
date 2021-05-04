a9de40f8f5879bccba82c5fd208863c3af6bab7f
h2tomohiro <h2.adachi.tomohiro@gmail.com>
2021-05-04 15:14:14 -0700

    Done hero section

--- components/lp/Hero.tsx
+++ components/lp/Hero.tsx
@@ -5,12 +5,14 @@ const Hero = () => {
     <div className="relative h-screen flex items-center">
       <Image src="/images/hero.svg" layout="fill" objectFit="cover" />
       <div className="relative container">
-        <p>
+        <p className="text-5xl font-bold text-black leading-relaxed mb-6">
           おい、イベントを
           <br />
           はじめよう
         </p>
-        <a>アカウント作成</a>
+        <a className="font-bold bg-white py-4 px-10 rounded-full shadow">
+          アカウント作成
+        </a>
       </div>
     </div>
   );
