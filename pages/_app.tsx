b3b054a1d34d24b09f39aa51e5b589cd2154008e
h2tomohiro <h2.adachi.tomohiro@gmail.com>
2021-05-04 17:09:01 -0700

    Add footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import "tailwindcss/tailwind.css";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <footer className="border-t">
+        <div className="container">
+          <p>&copy; event</p>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default MyApp;
